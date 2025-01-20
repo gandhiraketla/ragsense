412eb96edae4a3c066b2e04c52b7da75c0071600
Gandhi.R <gandhi.raketla@gmail.com>
2025-01-20 11:21:34 -0600

    Watch Folder and Kafka producer

--- backend/connectors/local_file_system_connector.py
+++ backend/connectors/local_file_system_connector.py
@@ -2,7 +2,6 @@ import json
 import sys
 import os
 from datetime import datetime
-
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if parent_dir not in sys.path:
     sys.path.insert(0, parent_dir)
